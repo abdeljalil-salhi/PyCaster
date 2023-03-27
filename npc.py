12de96cb3214e789897e0c5e71c2104448c0186b
Abdel <abdelisloading@gmail.com>
2023-03-27 00:46:46 +0000

    Set better settings

--- npc.py
+++ npc.py
@@ -191,7 +191,7 @@ class CacoDemonNPC(NPC):
     def __init__(self, game, path='resources/sprites/npc/caco_demon/0.png', pos=(10.5, 6.5),
                  scale=0.7, shift=0.27, animation_time=250):
         super().__init__(game, path, pos, scale, shift, animation_time)
-        self.attack_dist = 1.0
+        self.attack_dist = 1
         self.health = 150
         self.attack_damage = 25
         self.speed = 0.05
