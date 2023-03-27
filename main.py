1407c54eeba8e1bdf399e70735766d8bd108708d
Abdel <abdelisloading@gmail.com>
2023-03-27 00:51:47 +0000

    Fix small multiple sound bug

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ class Game:
         self.global_trigger = False
         self.global_event = pg.USEREVENT + 0
         pg.time.set_timer(self.global_event, 40)
+        self.loop = self.sound.theme.play()
         self.new_game()
   
     def new_game(self):
